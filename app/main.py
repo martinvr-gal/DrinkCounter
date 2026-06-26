6842c9b94dd50f045e0849158dfac5b410f60047
martinvr-gal <martinvrandroid@gmail.com>
2026-06-27 00:22:38 +0200

    Feature/spotify api (#1)
    * Modificacións para incluír spotify api en vez de embebed
    
    * Api info en variables de entorno
    
    * Novos requerimentos
    
    * Comprobación temporal
    
    * Spotify API completo
    
    * Corrixido auth

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import sys
+import sys, os
 from pathlib import Path
 
 if __package__ is None or __package__ == "":
@@ -16,11 +16,17 @@ from starlette.requests import Request
 from app.schemas import CounterChangeRequest, CounterResponse, CounterSetRequest
 from app.service import CounterService
 
+from fastapi.responses import RedirectResponse
+from starlette.requests import Request
+from spotipy.oauth2 import SpotifyOAuth
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "data"
 DB_PATH = DATA_DIR / "counter.db"
 TEMPLATES_DIR = BASE_DIR / "app" / "templates"
 STATIC_DIR = BASE_DIR / "app" / "static"
+
+spotify_tokens = {}
 
 app = FastAPI(title="DrinkCounter")
 app.add_middleware(
@@ -32,6 +38,19 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 service = CounterService(DB_PATH)
+
+sp_oauth = SpotifyOAuth(
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
+    scope=(
+        "streaming "
+        "user-read-email "
+        "user-read-private "
+        "user-modify-playback-state "
+        "user-read-playback-state"
+    )
+)
 
 
 def get_service() -> CounterService:
@@ -92,6 +111,47 @@ def set_counter(
     except ValueError as exc:
         raise HTTPException(status_code=400, detail=str(exc)) from exc
     return CounterResponse(value=value)
+
+
+@app.get("/login")
+def login():
+    return RedirectResponse(
+        url=sp_oauth.get_authorize_url()
+    )
+
+
+@app.get("/callback")
+def callback(request: Request):
+
+    code = request.query_params.get("code")
+
+    token_info = sp_oauth.get_access_token(code)
+
+    spotify_tokens["token"] = token_info
+
+    return RedirectResponse("/tv")
+
+
+@app.get("/token")
+def get_token():
+
+    token = spotify_tokens.get("token")
+
+    if not token:
+        raise HTTPException(
+            status_code=401,
+            detail="Spotify non autenticado"
+        )
+
+    return {
+        "access_token":
+        token["access_token"]
+    }
+
+
+@app.get("/has-token")
+def has_token():
+    return {"ok": "token" in spotify_tokens}
 
 
 if __name__ == "__main__":
